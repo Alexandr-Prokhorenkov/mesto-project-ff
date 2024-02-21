b39bd0a7d70fe83198599471cb53be394502399c
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2024-02-21 11:28:04 +0300

    commit_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const container = document.querySelector('.page__content');
+const cardsContainer = container.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function onDelete(event) {
+  event.target.closest('.card').remove()
+}
 
-// @todo: Функция удаления карточки
+function addfunc(cardItem) {
+  const cardTempale = document.querySelector('#card-template').content;
+  const cardElement = cardTempale.querySelector('.places__item').cloneNode(true);
+  const delBtn = cardElement.querySelector('.card__delete-button');
+  delBtn.addEventListener('click', onDelete);
+  cardElement.querySelector('.card__title').textContent = cardItem.name;
+  cardElement.querySelector('.card__image').src = cardItem.link;
+  cardElement.querySelector('.card__image').alt = cardItem.description;
+  cardsContainer.append(cardElement);
+}
 
-// @todo: Вывести карточки на страницу
+for(i=0;i<initialCards.length;i++) {
+  cardItem = addfunc(initialCards[i]);
+}
+
