923f1763e9f40196a8dc0d60aa42a72e1d999deb
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2024-02-21 19:15:09 +0300

    commit_2

--- scripts/index.js
+++ scripts/index.js
@@ -6,18 +6,19 @@ function onDelete(event) {
   event.target.closest('.card').remove()
 }
 
-function addfunc(cardItem) {
+function addfunc(cardItem, deleteCallBack) {
   const cardTempale = document.querySelector('#card-template').content;
   const cardElement = cardTempale.querySelector('.places__item').cloneNode(true);
   const delBtn = cardElement.querySelector('.card__delete-button');
-  delBtn.addEventListener('click', onDelete);
+  delBtn.addEventListener('click', deleteCallBack);
   cardElement.querySelector('.card__title').textContent = cardItem.name;
   cardElement.querySelector('.card__image').src = cardItem.link;
   cardElement.querySelector('.card__image').alt = cardItem.description;
-  cardsContainer.append(cardElement);
+  return cardElement
 }
 
-for(i=0;i<initialCards.length;i++) {
-  cardItem = addfunc(initialCards[i]);
+for(let i=0;i<initialCards.length;i++) {
+  const cardItem = addfunc(initialCards[i], onDelete);
+  cardsContainer.append(cardItem);
 }
 
